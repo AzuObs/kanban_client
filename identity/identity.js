dd2f8851b6f0b0f6ef60f356a90ef67b9fc63d9c
AzuObs <dleaverfr@gmail.com>
2015-11-24 15:06:58 +0700

    fix bug with identifyService variable mispelled

---
+++ identity/identity.js
@@ -0,0 +1,102 @@
+(function() {
+	"use strict";
+
+	var identityMod = angular.module("identityModule", ["ngResource"]);
+
+	identityMod.config(function($httpProvider, $stateProvider) {
+		$httpProvider.interceptors.push(function() {
+			return {
+				request: function(req) {
+					if (sessionStorage.token) {
+						req.headers.token = sessionStorage.token;
+					}
+					return req;
+				}
+			};
+		});
+
+		$stateProvider.state("identity", {
+			url: "/identity",
+			templateUrl: "identity/identity.html",
+			controller: "identityCtrl"
+		});
+	});
+
+
+	identityMod.service("identityService", function($http, $rootScope, $q) {
+		var service = this;
+
+		service.createUser = function(params) {
+			var defer = $q.defer();
+			$http
+				.post($rootScope.endPoint + "/user", params)
+				.success(function(res) {
+					defer.resolve(res);
+				})
+				.error(function(err) {
+					defer.reject(err);
+				});
+
+			return defer.promise;
+		};
+
+		service.authenticate = function(params) {
+			var defer = $q.defer();
+			$http
+				.post($rootScope.endPoint + "/user/loggin", params)
+				.success(function(res) {
+					defer.resolve(res);
+				})
+				.error(function(err) {
+					defer.reject(err);
+				});
+
+			return defer.promise;
+		};
+
+		return service;
+	});
+
+
+	identityMod.controller("identityCtrl", function($scope, identityService, $state) {
+		$scope.newAccUsr = "";
+		$scope.newAccPwd = "";
+		$scope.newAccPwdVerify = "";
+
+		$scope.logginUsername = "daniel";
+		$scope.logginPwd = "123";
+
+		$scope.createUser = function() {
+			identityService
+				.createUser({
+					username: $scope.newAccUsr,
+					pwd: $scope.newAccPwd
+				})
+				.then(function(res) {
+					sessionStorage.token = res.token;
+					$state.go("kanban.boardList", {
+						username: $scope.newAccUsr
+					});
+				}, function(err) {
+					console.log(err);
+				});
+		};
+
+
+		$scope.authenticate = function() {
+			identityService
+				.authenticate({
+					username: $scope.logginUsername,
+					pwd: $scope.logginPwd
+				})
+				.then(function(res) {
+					sessionStorage.token = res.token;
+					$state.go("kanban.boardList", {
+						username: $scope.logginUsername
+					});
+				}, function(err) {
+					console.log(err);
+				});
+		};
+	});
+})();
