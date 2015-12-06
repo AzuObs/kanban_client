af858c50d5b58fdeadf1a9febbe6a554f13146ce
AzuObs <dleaverfr@gmail.com>
2015-12-06 10:29:11 +0700

    fix placeholder height

--- kanban/js/board.category.js
+++ kanban/js/board.category.js
@@ -44,16 +44,21 @@
 		};
 
 		$scope.categorySortOpts = {
-			stop: function(e, ui) {
-				$scope.updateBoard();
-			},
 			horizontal: true,
 			tolerance: "pointer",
 			distance: 1,
 			cursor: "move",
 			opacity: 0.3,
 			scroll: true,
-			scrollSensitivity: 20
+			scrollSensitivity: 20,
+			activate: function(e, ui) {
+				//fix height of placeholder
+				var height = $(ui.item[0].children[0]).css("height");
+				$(ui.placeholder[0]).css("height", height);
+			},
+			stop: function(e, ui) {
+				$scope.updateBoard();
+			}
 		};
 	}]);
 
