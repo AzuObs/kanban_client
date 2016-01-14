e7cedc423ae8eee622965e7527bc5a8507673aa4
AzuObs <dleaverfr@gmail.com>
2016-01-14 16:13:46 +0700

    remove comments

--- e2e/conf.js
+++ e2e/conf.js
@@ -2,11 +2,11 @@ exports.config = {
 	framework: "jasmine",
 	seleniumAddress: "http://localhost:4444/wd/hub",
 	suites: {
-		navbar: "suites/navbar/navbar-e2e.js", //ok 8 
-		footer: "suites/footer/footer-e2e.js", //ok 4
-		about: "suites/about/about-e2e.js", //ok 6
-		oauth: "suites/oauth/oauth-e2e.js", //ok 6
-		boardList: "suites/board-list/board-list-e2e.js", // 25th?
+		navbar: "suites/navbar/navbar-e2e.js",
+		footer: "suites/footer/footer-e2e.js",
+		about: "suites/about/about-e2e.js",
+		oauth: "suites/oauth/oauth-e2e.js",
+		boardList: "suites/board-list/board-list-e2e.js",
 		boardModal: "suites/board-modal/board-modal-e2e.js",
 		board: "suites/board/board-e2e.js",
 		taskModal: "suites/task-modal/task-modal-e2e.js",
