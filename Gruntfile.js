2279ed1c9eee6136c7e430eb42098669b3463865
AzuObs <dleaverfr@gmail.com>
2016-01-16 18:50:35 +0700

    config grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,45 +10,87 @@ module.exports = function(grunt) {
         autoWatch: true
       }
     },
-    concat: {
-      css: {
-        src: [
-          "src/**/*.css",
-          "!src/bower_components/**/*.css"
-        ],
-        dest: "release/main.css",
-        stripBanners: true
-      },
-      js: {
-        src: [
-          "src/**/*.js",
-          "!src/**/*test.js",
-          "!src/bower_components/**/*.js"
-        ],
-        dest: "release/main.js",
-        stripBanners: true
 
+    // CONCAT, UGLIFY, CSSMIN, AND MODIFY REFERENCES IN INDEX.HTML
+    useminPrepare: {
+      html: "src/index.html",
+      options: {
+        dest: "release"
       }
     },
-    uglify: {
-      src: "release/main.js",
-      dest: "release/main.js"
+    usemin: {
+      html: "release/index.html"
     },
-    cssmin: {
-      target: {
-        src: "release/main.css",
-        dest: "release/main.css"
+
+    // COPY
+    copy: {
+      html: {
+        expand: true,
+        cwd: "src/",
+        src: "**/*.html",
+        dest: "release/"
+      },
+      bower_components_min: {
+        expand: true,
+        cwd: "src/bower_components",
+        src: [
+          "angular/angular.min.js", //CDN
+          "bootstrap/dist/css/bootstrap.min.css", //CDN
+          "jquery/dist/jquery.min.js" //CDN
+        ],
+        dest: "release/bower_components"
+      }
+    },
+
+    // MINIFICATION HTML
+    htmlmin: {
+      build: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {
+          "release/index.html": "release/index.html",
+          "release/oauth/oauth.html": "release/oauth/oauth.html",
+          "release/common/footer/footer.html": "release/common/footer/footer.html",
+          "release/common/navbar/navbar.html": "release/common/navbar/navbar.html",
+          "release/common/state-info/state-info.html": "release/common/state-info/state-info.html",
+          "release/board-list/board-list.html": "release/board-list/board-list.html",
+          "release/board-list/board-modal/board-modal.html": "release/board-list/board-modal/board-modal.html",
+          "release/board/board.html": "release/board/board.html",
+          "release/board/users/users.html": "release/board/users/users.html",
+          "release/board/users/user-directive/user-directive.html": "release/board/users/user-directive/user-directive.html",
+          "release/board/users/user-modal/user-modal.html": "release/board/users/user-modal/user-modal.html",
+          "release/board/tasks/tasks.html": "release/board/tasks/tasks.html",
+          "release/board/tasks/task-directive/task-directive.html": "release/board/tasks/task-directive/task-directive.html",
+          "release/board/tasks/task-modal/task-modal.html": "release/board/tasks/task-modal/task-modal.html",
+          "release/board/categories/categories.html": "release/board/categories/categories.html",
+          "release/board/categories/category-directive/category-directive.html": "release/board/categories/category-directive/category-directive.html",
+          "release/about/about.html": "release/about/about.html"
+        }
       }
     }
   });
 
   grunt.loadNpmTasks("grunt-karma");
+
   grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-copy");
   grunt.loadNpmTasks("grunt-contrib-cssmin");
   grunt.loadNpmTasks("grunt-contrib-htmlmin");
   grunt.loadNpmTasks("grunt-contrib-uglify");
   grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-usemin");
+  grunt.loadNpmTasks("grunt-filerev");
 
   grunt.registerTask("unit-test", ["karma"]);
-  grunt.registarTask("build", ["concat", "uglify", "cssmin"]);
+  grunt.registerTask("build", [
+    "copy",
+    "useminPrepare",
+    "concat:generated",
+    "uglify:generated",
+    "cssmin:generated",
+    "usemin"
+    // "htmlmin"
+  ]);
 };
