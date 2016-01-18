672723dcafc67a1a4485df105df8b31bd466cb9a
AzuObs <dleaverfr@gmail.com>
2016-01-18 16:58:54 +0700

    refactor kbUser directive

--- src/board/users/user-directive/user-directive.js
+++ src/board/users/user-directive/user-directive.js
@@ -4,10 +4,24 @@
 	var module = angular.module("userDirectiveModule", ["ui.bootstrap"]);
 
 	module.directive("kbUser", function() {
-		return {
+
+		//best practice
+		var directiveDefinitionObject = {
 			restrict: "E",
+			scope: {
+				user: "=ngModel"
+			},
 			replace: true,
-			templateUrl: "board/users/user-directive/user-directive.html"
+			templateUrl: "board/users/user-directive/user-directive.html",
+			link: function(scope, elem, attr) {
+
+				//best practice
+				scope.$on("$destroy", function() {
+
+				});
+			}
 		};
+
+		return directiveDefinitionObject;
 	});
 })();
