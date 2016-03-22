9f39fb79bb7a705762e66644bc32166d5e1c56b1
AzuObs <dleaverfr@gmail.com>
2016-03-22 10:54:22 +0100

    refactor index-test.js now that I better understand how DI works

--- src/index-test.js
+++ src/index-test.js
@@ -1,65 +1,66 @@
 (function() {
-	"use strict";
+  "use strict";
 
-	describe("kanbanApp module", function() {
-		var debugInfoEnabled, cfpLoadingBarProvider, $rootScope;
+  describe("kanbanApp module", function() {
+    var debugInfoEnabled, cfpLoadingBarProvider, $rootScope;
 
-		// the following is a hack
-		// in order to test the config phase of my target module 
-		// I need to set the spies up before .config has been executed		
-		beforeEach(function() {
-			module("aboutModule", function($compileProvider) {
-				spyOn($compileProvider, "debugInfoEnabled").and.callFake(function() {
-					debugInfoEnabled = arguments[0];
-				});
-			});
-			module("kanbanApp", function(_cfpLoadingBarProvider_) {
-				cfpLoadingBarProvider = _cfpLoadingBarProvider_;
-			});
-
-			inject(function(_$rootScope_) {
-				// this has to be here even if we do not inject anything
-				// or else cfpLoadingBarProvider and debugInfoEnabled will
-				// be undefined
-				$rootScope = _$rootScope_;
-			});
-		});
+    beforeEach(function() {
+      // in order to test the config phase of "kanbanApp" module
+      // we need to create a module with a config function so that we can access the providers that interest us		
+      angular.module("configModule", ["angular-loading-bar"]).config(function($compileProvider, _cfpLoadingBarProvider_) {
+        spyOn($compileProvider, "debugInfoEnabled").and.callFake(function() {
+          debugInfoEnabled = arguments[0];
+        });
+        cfpLoadingBarProvider = _cfpLoadingBarProvider_;
+      });
+      // we then need to load our new module BEFORE we load the kanbanApp module in order for our spy to trigger
+      // during the loading of the angular module
+      angular.module("sequencingModule", ["configModule", "kanbanApp"]);
+      // then we will mock our module
+      module("mySecondModule");
 
 
-		describe("config phase", function() {
-			describe("debugInfoEnabled", function() {
-				it("is enabled during production", function() {
-					expect(debugInfoEnabled).toEqual(true);
-				});
-			});
-
-			describe("cfpLoadingBarProvider", function() {
-				it("has a disabled spinner", function() {
-					expect(cfpLoadingBarProvider.includeSpinner).toEqual(false);
-				});
-
-				it("has a latency treshold to 250ms", function() {
-					expect(cfpLoadingBarProvider.latencyTreshold).toEqual(250);
-				});
-			});
-		});
+      inject(function(_$rootScope_) {
+        // and finally we need to do the DI and loads the modules, and providers into the caches
+        $rootScope = _$rootScope_;
+      });
+    });
 
 
-		describe("run phase", function() {
-			describe("$rootScope.websiteTitle", function() {
-				it("is defined", function() {
-					expect($rootScope.websiteTitle).toBeDefined();
-				});
+    describe("config phase", function() {
+      describe("debugInfoEnabled", function() {
+        it("is enabled during production", function() {
+          expect(debugInfoEnabled).toEqual(true);
+        });
+      });
 
-				it("is a string", function() {
-					expect(typeof $rootScope.websiteTitle).toEqual("string");
-				});
+      describe("cfpLoadingBarProvider", function() {
+        it("has a disabled spinner", function() {
+          expect(cfpLoadingBarProvider.includeSpinner).toEqual(false);
+        });
+
+        it("has a latency treshold to 250ms", function() {
+          expect(cfpLoadingBarProvider.latencyTreshold).toEqual(250);
+        });
+      });
+    });
 
 
-				it("it is equal to ENV.version", inject(function(ENV) {
-					expect($rootScope.websiteTitle).toEqual("Kanban " + ENV.websiteVersion);
-				}));
-			});
-		});
-	});
+    describe("run phase", function() {
+      describe("$rootScope.websiteTitle", function() {
+        it("is defined", function() {
+          expect($rootScope.websiteTitle).toBeDefined();
+        });
+
+        it("is a string", function() {
+          expect(typeof $rootScope.websiteTitle).toEqual("string");
+        });
+
+
+        it("it is equal to ENV.version", inject(function(ENV) {
+          expect($rootScope.websiteTitle).toEqual("Kanban " + ENV.websiteVersion);
+        }));
+      });
+    });
+  });
 })();
--- src/index.js
+++ src/index.js
@@ -1,59 +1,59 @@
 (function() {
-	"use strict";
+  "use strict";
 
-	var module = angular.module("kanbanApp", [
-		"ui.router",
-		"aboutModule",
-		"boardModule",
-		"boardListModule",
-		"oauthModule",
-		"navbarModule",
-		"angular-loading-bar",
-		"errorPageModule",
-		"errorHandlerModule",
-		"copyChildMinHeightDirectiveModule",
-		"environmentModule"
-	]);
-
-
-	module.config([
-		"$stateProvider", "$urlRouterProvider", "$compileProvider", "$injector", "ENV",
-		"cfpLoadingBarProvider",
-		function($stateProvider, $urlRouterProvider, $compileProvider, $injector, ENV,
-			cfpLoadingBarProvider) {
-
-			$stateProvider.state("kanban", {
-				abstract: true,
-				url: ""
-			});
-
-			// default redirects
-			$urlRouterProvider.when("", "/identity");
-			$urlRouterProvider.otherwise(function($injector) {
-				$injector.invoke(["errorHandler", function(errorHandler) {
-					errorHandler.handleAppError("Not Found");
-				}]);
-			});
-
-			// debugApp === true during development, === false during deployment
-			$compileProvider.debugInfoEnabled(ENV.debugApp);
-
-			cfpLoadingBarProvider.includeSpinner = false;
-			cfpLoadingBarProvider.latencyTreshold = 250;
-		}
-	]);
+  var module = angular.module("kanbanApp", [
+    "ui.router",
+    "aboutModule",
+    "boardModule",
+    "boardListModule",
+    "oauthModule",
+    "navbarModule",
+    "angular-loading-bar",
+    "errorPageModule",
+    "errorHandlerModule",
+    "copyChildMinHeightDirectiveModule",
+    "environmentModule"
+  ]);
 
 
-	// 'run' cannot be replaced by 'controller' because it will only give scope to the .body-view 
-	// in the HTML, except that the TITLE of the web page needs to be the website version... hence why
-	// we need the scope to be greater than .body-view the whole ng-app
-	module.run([
-		"$rootScope", "ENV", "errorHandler",
-		function($rootScope, ENV, errorHandler) {
-			$rootScope.websiteTitle =  "Kanban " + ENV.websiteVersion;
-			$rootScope.$on("$stateChangeError", function() {
-				errorHandler.handleAppError("State Change Error");
-			});
-		}
-	]);
+  module.config([
+    "$stateProvider", "$urlRouterProvider", "$compileProvider", "$injector", "ENV",
+    "cfpLoadingBarProvider",
+    function($stateProvider, $urlRouterProvider, $compileProvider, $injector, ENV,
+      cfpLoadingBarProvider) {
+
+      $stateProvider.state("kanban", {
+        abstract: true,
+        url: ""
+      });
+
+      // default redirects
+      $urlRouterProvider.when("", "/identity");
+      $urlRouterProvider.otherwise(function($injector) {
+        $injector.invoke(["errorHandler", function(errorHandler) {
+          errorHandler.handleAppError("Not Found");
+        }]);
+      });
+
+      // debugApp === true during development, === false during deployment
+      $compileProvider.debugInfoEnabled(ENV.debugApp);
+
+      cfpLoadingBarProvider.includeSpinner = false;
+      cfpLoadingBarProvider.latencyTreshold = 250;
+    }
+  ]);
+
+
+  // 'run' cannot be replaced by 'controller' because it will only give scope to the .body-view 
+  // in the HTML, except that the TITLE of the web page needs to be the website version... hence why
+  // we need the scope to be greater than .body-view the whole ng-app
+  module.run([
+    "$rootScope", "ENV", "errorHandler",
+    function($rootScope, ENV, errorHandler) {
+      $rootScope.websiteTitle = "Kanban " + ENV.websiteVersion;
+      $rootScope.$on("$stateChangeError", function() {
+        errorHandler.handleAppError("State Change Error");
+      });
+    }
+  ]);
 })();
