aab74b315ab87b34924073f62fb19456a75b3401
AzuObs <dleaverfr@gmail.com>
2016-04-20 15:14:06 +0200

    fix login-help box min-height on oauth-page.css

--- src/about-page/about.js
+++ src/about-page/about.js
@@ -1,33 +1,33 @@
 (function() {
-	"use strict";
+  "use strict";
 
-	var module = angular.module("aboutModule", [
-		"navbarModule",
-		"ui.router"
-	]);
+  var module = angular.module("aboutModule", [
+    "navbarModule",
+    "ui.router"
+  ]);
 
-	module.config(["$stateProvider", function($stateProvider) {
-		$stateProvider.state("kanban.about", {
-			views: {
-				"navbar-view@": {
-					templateUrl: "common/views/navbar/navbar.html",
-					controller: "navbarCtrl"
-				},
-				"body-view@": {
-					templateUrl: "about-page/about.html",
-					controller: "aboutCtrl"
-				},
-				"footer-view@": {
-					templateUrl: "common/views/footer/footer.html"
-				}
-			},
-			url: "/about"
-		});
-	}]);
+  module.config(["$stateProvider", function($stateProvider) {
+    $stateProvider.state("kanban.about", {
+      views: {
+        "navbar-view@": {
+          templateUrl: "common/views/navbar/navbar.html",
+          controller: "navbarCtrl"
+        },
+        "body-view@": {
+          templateUrl: "about-page/about.html",
+          controller: "aboutCtrl"
+        },
+        "footer-view@": {
+          templateUrl: "common/views/footer/footer.html"
+        }
+      },
+      url: "/about"
+    });
+  }]);
 
-	module.controller("aboutCtrl", ["$scope", function($scope) {
-		$scope.resources = ["angularjs", "angular-ui-router", "angular-ui-bootstrap", "angular-ui-sortable",
-			"angular-loading-bar", "bootstrap.css", "nodejs", "expressjs", "mongodb", "mongoose", "github", "heroku"
-		];
-	}]);
+  module.controller("aboutCtrl", ["$scope", function($scope) {
+    $scope.resources = ["angularjs", "angular-ui-router", "angular-ui-bootstrap", "angular-ui-sortable",
+      "angular-loading-bar", "bootstrap.css", "nodejs", "expressjs", "mongodb", "mongoose", "git", "grunt", "karma", "jasmine"
+    ];
+  }]);
 })();
